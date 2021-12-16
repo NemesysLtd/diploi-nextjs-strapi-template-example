c29b886c208fb8832940d4be3cd8707f7d4be410
Tuomas Pöyry <tuomas@munkkeli.io>
2021-12-16 16:14:11 +0200

    SQLTools config

--- api/config/database.js
+++ api/config/database.js
@@ -6,8 +6,8 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("POSTGRES_HOST", "localhost"),
       port: env("POSTGRES_PORT", 5432),
-      user: env("POSTGRES_USER", "postgres"),
-      password: env("POSTGRES_HOST", ""),
+      user: env("POSTGRES_USER", "strapi"),
+      password: env("POSTGRES_PASSWORD", "strapi"),
       database: env("POSTGRES_HOST", "strapi"),
     },
     useNullAsDefault: true,
