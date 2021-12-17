97255b22d8b659ed4e6aefbc19338b31bf3c984c
Tuomas Pöyry <tuomas@munkkeli.io>
2021-12-17 10:59:38 +0200

    Postgres config

--- api/config/database.js
+++ api/config/database.js
@@ -4,11 +4,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("POSTGRES_HOST", "localhost"),
+      host: env("POSTGRES_HOST", "postgres"),
       port: env("POSTGRES_PORT", 5432),
-      user: env("POSTGRES_USER", "strapi"),
-      password: env("POSTGRES_PASSWORD", "strapi"),
-      database: env("POSTGRES_HOST", "strapi"),
+      user: env("POSTGRES_USER", "postgres"),
+      password: env("POSTGRES_PASSWORD", "postgres"),
+      database: env("POSTGRES_DB", "strapi"),
     },
     useNullAsDefault: true,
   },
